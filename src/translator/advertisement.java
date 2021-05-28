4e68bef2444ebfcdc1217e81ebfe75d122b2a116
unknown <muhammadtahaazam@gmail.com>
2021-05-28 14:48:26 +0500

    Translational API in java

---
+++ src/translator/advertisement.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package translator;
+
+/**
+ *
+ * @author Zepcom
+ */
+public class advertisement {
+    
+    
+    
+    public void modeofAds(){
+        System.out.println("Select any mode of advertisement");
+        System.out.println("1- SMS\n2- Mail\n3- Cable-Tv"); 
+   } 
+    
+}
